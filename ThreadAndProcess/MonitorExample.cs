ed0c5f7ee4b59408d1fbb17b2047f5a6d3624715
DeniOSI <geksabyte@gmail.com>
2016-10-12 20:04:05 +0300

    Monitor Wait() and Pulse()

---
+++ ThreadAndProcess/MonitorExample.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ThreadAndProcess
+{
+    class MonitorExample //Тик так
+    {
+        public void Tick(bool running)
+        {
+            lock (this)
+            {
+                if (!running) { Monitor.Pulse(this); return; }
+                Console.WriteLine("tick");
+                Monitor.Pulse(this);
+                Monitor.Wait(this);
+
+            }
+
+        }
+
+
+        public void Tock(bool running)
+        {
+
+            lock (this)
+            {
+                if (!running) { Monitor.Pulse(this); return; }
+                Console.WriteLine("tock");
+                Monitor.Pulse(this);
+                Monitor.Wait(this); 
+            }
+            
+        }
+    }
+    class MyThreadMonitor
+    {
+        public Thread thrd;
+        MonitorExample ttob;
+        public MyThreadMonitor(string name, MonitorExample me)
+        {
+            thrd = new Thread(new ThreadStart(this.Run));
+            thrd.Name = name;
+            ttob = me;
+            thrd.Start();
+        }
+
+        public void Run()
+        {
+           
+           if(thrd.Name == "tick") {
+                for(int i = 0; i< 5; i++)
+                ttob.Tick(true); ttob.Tick(false);
+                Thread.Sleep(1000);
+            }
+           else { if (thrd.Name == "tock") { for (int i = 0; i < 5; i++)
+                     ttob.Tock(true); ttob.Tock(false);
+                    Thread.Sleep(1000);
+                }
+            }
+        }
+    }
+}
--- ThreadAndProcess/Program.cs
+++ ThreadAndProcess/Program.cs
@@ -34,16 +34,25 @@ namespace ThreadAndProcess
             //Console.WriteLine("Time = " + (dt.Second - minutes) + "seconds and " + (dt.Millisecond - milsec) + " millisecond"); 
             #endregion
 
-            #region Lock
-            int[] test_arr= {10, 200, 39, 21, 14, 61};
-            MyThread mt = new MyThread("First Thread", test_arr);
-            MyThread mt1 = new MyThread("Second Thread", test_arr);
-            mt1.thrd.Join();
+            #region Lock последовательное выполнение потоков в следствии обработки инструкции lock
+            //int[] test_arr= {10, 200, 39, 21, 14, 61};
+            //MyThread mt = new MyThread("First Thread", test_arr);
+            //MyThread mt1 = new MyThread("Second Thread", test_arr);
+            //mt1.thrd.Join();
 
-            mt.thrd.Join();
+            //mt.thrd.Join();
 
             #endregion
 
+            #region (Monitor) тик-так
+            MonitorExample me = new MonitorExample();
+            MyThreadMonitor mtm = new MyThreadMonitor("tick", me);
+            MyThreadMonitor mtm1 = new MyThreadMonitor("tock", me);
+       
+            mtm.thrd.Join();
+           
+            mtm1.thrd.Join();
+            #endregion
             Console.ReadKey();
         }
     }
