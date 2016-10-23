ac90684df3267371159a41c91455ed0c0f480d89
DeniOSI <geksabyte@gmail.com>
2016-10-23 11:26:50 +0300

    aasdfsdf

--- ThreadAndProcess/Program.cs
+++ ThreadAndProcess/Program.cs
@@ -10,28 +10,28 @@ namespace ThreadAndProcess
         static void Main(string[] args)
         {
             #region ThreadAndTimerinThread
-            //DateTime dt = DateTime.Now;
-            //int minutes = dt.Second;
-            //int milsec = dt.Millisecond;
-            //ThreadPriorityX lowerThread = new ThreadPriorityX("lower");
-            //lowerThread.thrd.Priority = System.Threading.ThreadPriority.Lowest;
-            //ThreadPriorityX aboou = new ThreadPriorityX("Abou");
-            //aboou.thrd.Priority = System.Threading.ThreadPriority.AboveNormal;
-            //TimerThread timer = new TimerThread("myTimer");
-            //timer.thrd.Priority = System.Threading.ThreadPriority.Normal;
-            //timer.thrd.Start();
-            //lowerThread.thrd.Start();
-            //aboou.thrd.Start();
-            //timer.thrd.Join();
-            //lowerThread.thrd.Join();
-            //aboou.thrd.Join();
-            //Console.WriteLine(
-            //    );
-            //Console.WriteLine("The thread is " + timer.thrd.Name);
-            //Console.WriteLine("The thread is " + lowerThread.thrd.Name + " count to = " + lowerThread.count);
-            //Console.WriteLine("The thread is " + aboou.thrd.Name + " count to = " + aboou.count);
-            //dt = DateTime.Now;
-            //Console.WriteLine("Time = " + (dt.Second - minutes) + "seconds and " + (dt.Millisecond - milsec) + " millisecond"); 
+            DateTime dt = DateTime.Now;
+            int minutes = dt.Second;
+            int milsec = dt.Millisecond;
+            ThreadPriorityX lowerThread = new ThreadPriorityX("lower");
+            lowerThread.thrd.Priority = System.Threading.ThreadPriority.Lowest;
+            ThreadPriorityX aboou = new ThreadPriorityX("Abou");
+            aboou.thrd.Priority = System.Threading.ThreadPriority.AboveNormal;
+            TimerThread timer = new TimerThread("myTimer");
+            timer.thrd.Priority = System.Threading.ThreadPriority.Normal;
+            timer.thrd.Start();
+            lowerThread.thrd.Start();
+            aboou.thrd.Start();
+            timer.thrd.Join();
+            lowerThread.thrd.Join();
+            aboou.thrd.Join();
+            Console.WriteLine(
+                );
+            Console.WriteLine("The thread is " + timer.thrd.Name);
+            Console.WriteLine("The thread is " + lowerThread.thrd.Name + " count to = " + lowerThread.count);
+            Console.WriteLine("The thread is " + aboou.thrd.Name + " count to = " + aboou.count);
+            dt = DateTime.Now;
+            Console.WriteLine("Time = " + (dt.Second - minutes) + "seconds and " + (dt.Millisecond - milsec) + " millisecond");
             #endregion
 
             #region Lock последовательное выполнение потоков в следствии обработки инструкции lock
